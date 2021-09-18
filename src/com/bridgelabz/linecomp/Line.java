acb120a5063623b65caaa2b77707293312bb5696
777shetty <shetty.chetu7@gmail.com>
2021-09-18 23:05:30 +0530

    ADDED:Points abject created outside the class

---
+++ src/com/bridgelabz/linecomp/Line.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomp;
+
+public class Line {
+
+	final Point firstPoint, secondPoint;
+
+	public Line(Point firstPoint, Point secondPoint) {
+		this.firstPoint = firstPoint;
+		this.secondPoint = secondPoint;
+	}
+}
+
